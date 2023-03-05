c4266b7305ff028ee971c3767ca2b77518b2671a
alakpodia <raymond.alakpodia@gmail.com>
2023-03-05 15:09:46 +0100

    Completed Functionality

---
+++ Unit1.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *TitleLabel;
+	TEdit *NumbersEdit;
+	TButton *ConfirmButton;
+	TLabel *HelperLabel;
+	TLabel *CounterLabel;
+	void __fastcall ConfirmButtonClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
